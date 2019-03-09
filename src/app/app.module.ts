7ac12faa13156a3be399e117d2e5d6d7e39af2a1
CchMark <start2008go@gmail.com>
2019-03-09 11:03:20 +0800

    現有模板切版，及元件規劃

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArticlesComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
